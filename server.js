7ba383883c1d3b0654102f4c25df763928b995c1
mangal182 <30900302+mangal182@users.noreply.github.com>
2017-08-16 22:22:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-1', function (req,res) {
-    res.send('Article 1 requested and will be served here');
+app.get('/article-one', function (req,res) {
+    res.send('Article one requested and will be served here');
 });
 
-app.get('/article-2', function (req,res) {
-    res.send('Article 2 requested and will be served here');
+app.get('/article-two', function (req,res) {
+    res.send('Article two requested and will be served here');
 });
 
 app.get('/article-3', function (req,res) {
