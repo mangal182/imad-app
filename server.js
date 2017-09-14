4f0ec6e1f324666f78c2644d0e9cbd03b3165f65
mangal182 <30900302+mangal182@users.noreply.github.com>
2017-09-14 23:19:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ function hash (input) {
 
 
 app.get('/hash/:input', function(req,res) {
-    var hashedString = hash(req, params, input);
+    var hashedString = hash(req, params, input, 'this is some random string');
     res.send(hashedString);
 });
 
