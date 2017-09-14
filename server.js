fc75b760617929354eb5864aa00e6711cc0c1640
mangal182 <30900302+mangal182@users.noreply.github.com>
2017-09-14 23:14:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var crypto = required('crypto');
 
 var app = express();
 app.use(morgan('combined'));
@@ -35,6 +36,20 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
+
+function hash (input) {
+    // How do we create a hash?
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512'); 
+    return hashed.toString('hex');
+}
+
+
+app.get('/hash/:input', function(req,res) {
+    var hashedString = hash(req, params, input);
+    res.send(hashedString);
+});
+
 
 var counter = 0;
 app.get('/counter', function (req,res) {
